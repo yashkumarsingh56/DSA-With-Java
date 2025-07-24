f685563246c594be239f4ffadc03fa8380e55539
yashkumarsingh56 <93072214+yashkumarsingh56@users.noreply.github.com>
2025-07-24 19:37:29 +0530

    Add files via upload

--- Anagram.java
+++ Anagram.java
@@ -1,25 +1,34 @@
 package ACCENTURE;
 
 public class Anagram {
-    public static boolean isAnagram(String S,String T){
-        if (S.length()!=T.length()){
-            return false;
-        }
-        int arr[] = new int[26];
-        for (int i=0;i<S.length();i++){
-            arr[S.charAt(i)-'a']++;
-            arr[T.charAt(i)-'a']--;
-        }
-        for (int i=0; i<26;i++){
-            if (arr[i]!=0){
-                return false;
-            }
-        }
-        return true;
+    public Anagram() {
     }
+
+    public static boolean isAnagram(String S, String T) {
+        if (S.length() != T.length()) {
+            return false;
+        } else {
+            int[] arr = new int[26];
+
+            int i;
+            for(i = 0; i < S.length(); ++i) {
+                ++arr[S.charAt(i) - 97];
+                --arr[T.charAt(i) - 97];
+            }
+
+            for(i = 0; i < 26; ++i) {
+                if (arr[i] != 0) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
     public static void main(String[] args) {
         String S = "yaash";
         String T = "aashy";
-        System.out.println(isAnagram(S,T));
+        System.out.println(isAnagram(S, T));
     }
 }
